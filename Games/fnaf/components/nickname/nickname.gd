e1dd191ab0dc4d6aef5189ff4cade43b35eb88fe
SimusDev <simusdev@mail.ru>
2025-05-25 22:19:32 +0300

    1

--- Games/fnaf/components/nickname/nickname.gd
+++ Games/fnaf/components/nickname/nickname.gd
@@ -3,6 +3,6 @@ extends Node3D
 @export var player: FNAF_Player
 
 func _ready() -> void:
+	visible = !player.is_multiplayer_authority()
 	var mp_player: SD_MultiplayerPlayer = SD_MultiplayerPlayer.find_in_node(player)
-	
 	$Label3D.text = mp_player.get_username()
