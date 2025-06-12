b36186fd8e729035d3dd53aa648831c8d3b3295a
SimusDev <simusdev@mail.ru>
2025-06-12 18:16:53 +0300

    console

--- addons/simusdev/trunk/SD_TrunkConsole.gd
+++ addons/simusdev/trunk/SD_TrunkConsole.gd
@@ -1,7 +1,7 @@
 extends SD_Console
 class_name SD_TrunkConsole
 
-var _console_prefab: PackedScene = preload("res://addons/simusdev/console/prefabs/ui_console_new.tscn")
+var _console_prefab: PackedScene = preload("res://addons/simusdev/console/prefabs/ui_console.tscn")
 var _debug_prefab: PackedScene = preload("res://addons/simusdev/debug/ui_debug_interface.tscn")
 
 var _console_node: Node = null
@@ -29,6 +29,8 @@ func _ready() -> void:
 	
 	if SD_Platforms.has_debug_console_feature():
 		_console_node = _console_prefab.instantiate()
+		if _console_node.has_method("set_visible"):
+			_console_node.visible = false
 		
 		_debug_node = _debug_prefab.instantiate()
 		
