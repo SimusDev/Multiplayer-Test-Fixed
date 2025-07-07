c3cdd5725c97eb26b7b11387b1523caa195c2bc4
SimusDev <simusdev@mail.ru>
2025-07-07 22:14:54 +0300

    inv and health upd

--- Games/experiment_life/components/inventory/EL_Inventory.gd
+++ Games/experiment_life/components/inventory/EL_Inventory.gd
@@ -1,14 +1,3 @@
 @icon("res://addons/simusdev/components/inventory/icon_inv.png")
 extends WG_Inventory
 class_name EL_Inventory
-
-var _spawner: SD_MPClientNodeSpawner
-
-func _ready() -> void:
-	super()
-	
-	_spawner = SD_MPClientNodeSpawner.new()
-	_spawner.start_name = "spawner"
-	_spawner.add_detect_root(self)
-	add_child(_spawner)
-	move_child(_spawner, 0)
