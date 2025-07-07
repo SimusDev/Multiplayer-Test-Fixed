7fe025e7708abb8cc1e969924a7d9d921ce6ca99
SimusDev <simusdev@mail.ru>
2025-07-07 11:53:04 +0300

    synced health component

--- scripts/components/health/C_HealthComponent.gd
+++ scripts/components/health/C_HealthComponent.gd
@@ -7,8 +7,14 @@ func _ready() -> void:
 		max_health_changed.connect(_on_server_max_health_changed)
 		return
 	
-	SD_Multiplayer.request_and_sync_var_from_server(self, "health")
-	SD_Multiplayer.request_and_sync_var_from_server(self, "max_health")
+	SD_Multiplayer.request_and_sync_var_from_server(self, "health", _var_health_sync)
+	SD_Multiplayer.request_and_sync_var_from_server(self, "max_health", _var_max_health_sync)
+
+func _var_health_sync() -> void:
+	_synchronize_health(health)
+
+func _var_max_health_sync() -> void:
+	_synchronize_max_health(max_health)
 
 func _on_server_health_changed() -> void:
 	SD_Multiplayer.sync_call_function(self, _synchronize_health, [health])
@@ -21,9 +27,11 @@ func _synchronize_health(synced: float) -> void:
 		return
 	
 	health = synced
+	health_changed.emit()
 
 func _synchronize_max_health(synced: float) -> void:
 	if SD_Multiplayer.is_server():
 		return
 	
 	max_health = synced
+	max_health_changed.emit()
