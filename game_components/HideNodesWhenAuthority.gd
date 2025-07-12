d32aea5c0e23e071c46f31d5d6cd90ebaf148d82
zxcvlad78 <143466931+zxcvlad78@users.noreply.github.com>
2025-07-12 21:20:55 +0300

    sex

--- game_components/HideNodesWhenAuthority.gd
+++ game_components/HideNodesWhenAuthority.gd
@@ -1,14 +1,19 @@
 extends Node
 
 @export var hide_at_start: bool = false
-@export var nodes: Array[Node]
+@export var authority_true: Array[Node]
+@export var authority_false: Array[Node]
 
 func _ready() -> void:
 	if hide_at_start:
 		hide_nodes()
 
 func hide_nodes() -> void:
-	for node in nodes:
+	for node in authority_true:
 		if node:
 			if node.is_multiplayer_authority() and node.has_method("hide"):
+					node.hide()
+	for node in authority_false:
+		if node:
+			if !node.is_multiplayer_authority() and node.has_method("hide"):
 					node.hide()
