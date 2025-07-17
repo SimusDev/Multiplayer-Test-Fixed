d89c11fb728f35cbc52a8667b3f9e054b920982c
zxcvlad78 <143466931+zxcvlad78@users.noreply.github.com>
2025-07-17 07:49:59 +0300

    1

--- Games/source_game/player/source_player.gd
+++ Games/source_game/player/source_player.gd
@@ -51,6 +51,7 @@ func _ready() -> void:
 		SourcePlayerUI.instance.update(health.health)
 
 func _on_state_enter(state:SD_State):
+	print(state.name)
 	model.tree.get("parameters/StateMachine/playback").travel(state.name)
 
 func set_model_blend():
