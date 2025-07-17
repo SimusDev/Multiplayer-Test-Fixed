628493c56a6d299bf43580f173ae4aa5f2e7bd49
zxcvlad78 <143466931+zxcvlad78@users.noreply.github.com>
2025-07-17 09:43:56 +0300

    1

--- sourcelike_interface/main_menu/game_name.gd
+++ sourcelike_interface/main_menu/game_name.gd
@@ -2,4 +2,4 @@ extends Control
 
 func _ready() -> void:
 	$name.text = ProjectSettings.get("application/config/name")
-	$devs.name = "by %s" % [SimusDev.get_settings().developer]
+	$devs.text = "by %s" % [SimusDev.get_settings().developer]
