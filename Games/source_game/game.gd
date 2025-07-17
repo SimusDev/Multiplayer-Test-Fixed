c4eb7b09f6dddb34c1ba7cc8853c2ef6cb0fe6d3
zxcvlad78 <143466931+zxcvlad78@users.noreply.github.com>
2025-07-17 08:20:11 +0300

    Update game.gd

--- Games/source_game/game.gd
+++ Games/source_game/game.gd
@@ -34,9 +34,12 @@ func add_freecam(_for:SD_MultiplayerPlayer):
 func request_spawn(prop_res:R_SourceProp):
 	SD_Multiplayer.call_func_on_server(spawn_on_server, [prop_res])
 
-func spawn_on_server(prop_res:R_SourceProp):
+func spawn_on_server(prop_res:R_SourceProp, peer_id:int):
 	var new_prop = prop_res.prefab.instantiate()
-	var spawn_pos = SourcePlayer.instance.interact_raycast.drag_item_link_node.global_position
+	
+	var player:SourcePlayer = SD_Multiplayer.get_player_by_peer_id(peer_id).get_player_node() as SourcePlayer
+	
+	var spawn_pos = player.interact_raycast.drag_item_link_node.global_position
 	SourceGame.instance.get_node("props").add_child(new_prop)
 	new_prop.global_position = spawn_pos
 	
