7bf03dea573073b23ca18022fdd6b29be2fa4059
SimusDev <simusdev@mail.ru>
2025-08-04 19:39:07 +0300

    builder and network fixes

--- addons/simusdev/network/singleton/SD_NetTrunkCallables.gd
+++ addons/simusdev/network/singleton/SD_NetTrunkCallables.gd
@@ -139,15 +139,19 @@ func call_func_on(peer: int, callable: Callable, args: Array = [], callmode: SD_
 		debug_print("failed to call unregistered function: %s, %s!, use SD_Network.register_function() for func registration" % [str(node), method], SD_ConsoleCategories.CATEGORY.ERROR)
 		return
 	
+	var channel_id: int = get_channel_by_name(channel)
 	
 	if peer == singleton.get_unique_id():
 		_remote_sender_id = peer
+		SD_Network.remote_sender.id = peer
+		SD_Network.remote_sender.channel = channel
+		SD_Network.remote_sender.channel_id = channel_id
+		SD_Network.remote_sender.callmode = callmode
+		SD_Network.remote_sender.player = SD_NetworkPlayer.get_by_peer_id(peer)
 		callable.callv(args)
 		return
 	
 	var base_class: String = _find_base_class(node)
-	
-	var channel_id: int = get_channel_by_name(channel)
 	
 	if channel_id > max_channels - 1:
 		debug_print("cant call func(%s) on channel %s, because id is greater than max channels: %s" % [method, channel, max_channels], SD_ConsoleCategories.CATEGORY.ERROR)
