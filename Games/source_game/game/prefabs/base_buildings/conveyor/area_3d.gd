77525490e54c180913d704fd57537a1b8f33be85
zxcvlad78 <143466931+zxcvlad78@users.noreply.github.com>
2025-08-15 11:57:03 +0300

    afs

--- Games/source_game/game/prefabs/base_buildings/conveyor/area_3d.gd
+++ Games/source_game/game/prefabs/base_buildings/conveyor/area_3d.gd
@@ -1,9 +1,15 @@
 extends Area3D
 
-var conveyor_speed:float = 1.0
-var conveyor_dir:Vector3 = Vector3(0, 0, -1)
-
+@export var conveyor_speed: float = 1.0
+@export var conveyor_dir: Vector3 = Vector3(0, 0, -1)
+@export var acceleration: float = 5.0
 func _process(delta: float) -> void:
+	var global_dir = transform.basis * conveyor_dir.normalized()
+	var target_velocity = global_dir * conveyor_speed
+	
 	for body in get_overlapping_bodies():
-		if body is RigidBody3D:
-			body.linear_velocity = (conveyor_dir * conveyor_speed) * transform.origin.z
+		if body is RigidBody3D and not body.is_sleeping():
+			body.linear_velocity = body.linear_velocity.lerp(
+				target_velocity, 
+				clamp(acceleration * delta, 0.0, 1.0)
+			)
