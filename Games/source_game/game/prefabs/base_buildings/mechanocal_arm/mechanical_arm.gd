8376932498c88cd88ec73bd3c0b0131f33079552
zxcvlad78 <143466931+zxcvlad78@users.noreply.github.com>
2025-08-16 21:21:14 +0300

    fa

--- Games/source_game/game/prefabs/base_buildings/mechanocal_arm/mechanical_arm.gd
+++ Games/source_game/game/prefabs/base_buildings/mechanocal_arm/mechanical_arm.gd
@@ -1,16 +1,19 @@
 class_name SourceMechanicalArm extends SourceBuilding
 
 signal current_target_changed
+signal target_reached
 
 enum ArmState {
 	IDLE,
 	MOVING_TO_TARGET,
 	GRABBING,
 	MOVING_TO_DROP,
-	DROPPING
+	DROPPING,
+	DROP
 }
 
 @export var main_target_node: Node3D
+@export var hand_target_node: Node3D
 @export var speed: float = 0.66
 
 @export_group("Drag Settings")
@@ -26,6 +29,7 @@ enum ArmState {
 @export var moving_audio_player: AudioStreamPlayer3D
 
 var current_target: Node3D = null : set = set_current_target
+var target_basis:Basis 
 var drop_target_confirmed: bool = false
 var current_state: ArmState = ArmState.IDLE : set = switch_state, get = get_current_state
 
@@ -38,11 +42,16 @@ func _ready() -> void:
 	area.body_entered.connect(_on_area_body_entered)
 	area.body_exited.connect(_on_area_body_exited)
 	current_target_changed.connect(_on_current_target_changed)
+	target_reached.connect(_on_target_reached)
+	animation_player.animation_finished.connect(_on_animation_finished)
 	drop_target_node.visible = false
 
 func _physics_process(delta: float) -> void:
-	if not SD_Network.is_server():
-		return
+	if current_target:
+		var direction = (current_target.global_position - main_target_node.global_position).normalized()
+		if direction:
+			target_basis = Basis.looking_at(direction, Vector3.UP)
+			main_target_node.global_transform.basis = target_basis
 
 func _on_area_body_entered(body:Node3D):
 	set_current_target(pick_target())
@@ -57,11 +66,15 @@ func switch_state(to:ArmState):
 	elif current_state == ArmState.MOVING_TO_TARGET:
 		pass
 	elif current_state == ArmState.GRABBING:
-		pass
+		animation_player.play(grab_anim_name)
 	elif current_state == ArmState.MOVING_TO_DROP:
 		pass
 	elif current_state == ArmState.DROPPING:
-		pass
+		animation_player.play_backwards(grab_anim_name)
+		await animation_player.animation_finished
+		switch_state(ArmState.DROP)
+	elif current_state == ArmState.DROP:
+		drop_item()
 
 func get_current_state() -> ArmState:
 	return current_state
@@ -70,17 +83,41 @@ func _on_current_target_changed() -> void:
 	if not is_instance_valid(current_target):
 		switch_state(ArmState.IDLE)
 		return
+	if not current_state == ArmState.IDLE:
+		return
 	
-	move_to(current_target.global_position)
+	move_to(current_target.global_position, "current_target")
 	switch_state(ArmState.MOVING_TO_TARGET)
 
-func move_to(_pos:Vector3) -> void:
-	get_tree().create_tween().tween_property(
+func move_to(_pos:Vector3, target_name:StringName) -> void:
+	moving_audio_player.play()
+	main_target_node.look_at(_pos, Vector3.MODEL_FRONT, true)
+	await get_tree().create_tween().tween_property(
 		main_target_node,
 		"global_position",
 		_pos,
-		speed
-	)
+		speed 
+	).finished
+	target_reached.emit(target_name)
+
+func _on_animation_finished(anim_name:String) -> void:
+	switch_state(ArmState.IDLE)
+
+func _on_target_reached(target_name:StringName) -> void:
+	print(target_name)
+	if target_name == "current_target": grab_and_move_item()
+	elif target_name == "drop": switch_state(ArmState.DROPPING)
+
+func grab_and_move_item() -> void:
+	switch_state(ArmState.GRABBING)
+	await animation_player.animation_finished
+	switch_state(ArmState.MOVING_TO_DROP)
+	move_to(drop_target_node.global_position, "drop")
+
+func drop_item() -> void:
+	current_state = ArmState.IDLE
+	current_target = null
+	set_current_target(pick_target())
 
 func set_current_target(_target:Node3D) -> void:
 	current_target = _target
