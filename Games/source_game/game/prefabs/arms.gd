7b67ede3ad7f0803b8d165b444379a5774371ed6
zxcvlad78 <143466931+zxcvlad78@users.noreply.github.com>
2025-09-05 15:01:33 +0300

    78

--- Games/source_game/game/prefabs/arms.gd
+++ Games/source_game/game/prefabs/arms.gd
@@ -2,10 +2,9 @@ extends Node3D
 
 @export var hands_ik:Array[SkeletonIK3D]
 
-func _ready() -> void:
+func _enter_tree() -> void:
 	initialize()
 
 func initialize() -> void:
 	for hand in hands_ik:
-		hand.stop()
 		hand.start()
