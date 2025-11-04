090cd5e54faea66d49de460169e3ff83b4cc7453
SimusDev <simusdev@mail.ru>
2025-11-04 20:59:26 +0300

    itemstack -_-

--- Games/source_game/components/inventory/source_itemstack_settings.gd
+++ Games/source_game/components/inventory/source_itemstack_settings.gd
@@ -4,6 +4,8 @@ class_name SourceItemStackSettings
 @export var stackable: bool = true
 @export var stack_size: int = 64
 @export var pickable: bool = true
+@export var durability: float = 0.0
+@export var durability_max: float = 0.0
 @export var custom_script: GDScript : get = get_custom_script
 @export var actions: Array[SourceItemAction] = [] : get = get_actions
 
