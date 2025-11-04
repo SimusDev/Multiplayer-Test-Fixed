4d97477d79d69cd3377cea1e8958abfd58ba91d4
zxcvlad78 <143466931+zxcvlad78@users.noreply.github.com>
2025-11-04 22:22:03 +0300

    Update ciga.gd

--- Games/source_game/items/ciga/item.gd
+++ Games/source_game/items/ciga/item.gd
@@ -3,13 +3,27 @@ extends SourceItem
 signal ignite_change
 
 @onready var progress_node:Node3D = $root/model/ciga/progress
+
 var ignite:bool = false : set = set_ignite, get = is_ignite
+
+@export var tickrate:float = 32.0
+@export var duration_per_tick:float = 0.1
 
 func _ready() -> void:
 	super()
 	
 	stack.durability_changed.connect(update)
 	set_ignite(true)
+
+	if SD_Network.is_server():
+		var tick_timer:Timer = Timer.new()
+		
+		tick_timer.wait_time = 1.0 / tickrate
+		tick_timer.timeout.connect(tick)
+		add_child(tick_timer)
+		
+		tick_timer.start()
+
 
 func set_ignite(value:bool) -> void:
 	ignite = value
@@ -19,6 +33,10 @@ func is_ignite() -> bool:
 	return ignite
 
 func update() -> void:
-	# min - 0.01; max - 0.055
 	var normalized_durability = stack.get_durability() / 100.0
 	progress_node.position.y = 0.01 + normalized_durability * 0.045
+
+func tick() -> void:
+	if stack.get_durability() >= duration_per_tick:
+		stack.set_durability( stack.get_durability() - duration_per_tick )
+	#update()
