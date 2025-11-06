c120f567c097beeb59428f6f2de9373291092aa5
SimusDev <simusdev@mail.ru>
2025-11-07 01:10:58 +0300

    fix

--- Games/source_game/components/entity_input.gd
+++ Games/source_game/components/entity_input.gd
@@ -15,8 +15,6 @@ func _ready() -> void:
 	
 	SD_Network.register_object(self)
 	SD_Network.register_functions([
-		_just_pressed_net,
-		_just_released_net,
 	])
 	
 	if SD_Network.is_authority(self):
@@ -24,32 +22,7 @@ func _ready() -> void:
 		_base.set_multiplayer_authority(get_multiplayer_authority())
 		_base.multiplayer_authorative = true
 		
-		_base.on_action_just_pressed.connect(_base_on_action_just_pressed)
-		_base.on_action_just_released.connect(_base_on_action_just_released)
-		
 		add_child(_base)
 	
 	SD_Components.append_to(root, self)
 	
-
-
-func _base_on_action_just_pressed(action: String, bind: SD_Keybind) -> void:
-	if !allowed_actions.is_empty() and !allowed_actions.has(action):
-		return
-	
-	SD_Network.call_func(_just_pressed_net, [SD_Network.singleton.cache.serialize_input_map(action)])
-
-func _base_on_action_just_released(action: String, bind: SD_Keybind) -> void:
-	if !allowed_actions.is_empty() and !allowed_actions.has(action):
-		return
-	
-	SD_Network.call_func(_just_released_net, [SD_Network.singleton.cache.serialize_input_map(action)])
-
-func _just_pressed_net(serialized: int) -> void:
-	var action: StringName = SD_Network.singleton.cache.deserialize_input_map(serialized)
-	action_just_pressed.emit(action)
-	
-
-func _just_released_net(serialized: int) -> void:
-	var action: StringName = SD_Network.singleton.cache.deserialize_input_map(serialized)
-	action_just_released.emit(action)
